58d8771434a8b3fbaa745bd4bdb6d1ee79b75cb5
choco1atebounty <104838053+choco1atebounty@users.noreply.github.com>
2022-06-07 04:16:12 +0300

    Update calc.js

--- calc/calc.js
+++ calc/calc.js
@@ -1,126 +1,187 @@
 "use strict"
-
 {
-let sait = [];
+let ver1;
+let ver2;
+let ver3;
+let ver4;
+let ver5;
+let ver6;
 
-let x;
-let y;
-let z;
+let tseni = {
 
+    tseniVerstka:
+    [
+        ver1 = ["Адаптивная", "Фиксированная", "Резиновая"]
+    ],
 
-let verstka = [" 1 - Фиксированная", " 2 - Резиновая", " 3 - Адаптивная"];
-prompt("Выбери тип верстки: " + verstka);
-let verstka2 = [];
-
-{
-    if (verstka == 1)
-    verstka2["Фиксированная"] = x,
-    x = 1111;
-}
-{
-    if (verstka == 2)
-    verstka2["Резиновая"] = x,
-    x = 2222;
-}
-{
-    if (verstka == 3)
-    verstka2["Адаптивная"] = x,
-    x = 5555;
-}
+    tseniSait: 
+    [
+        ver3 = ["Сайт-визитка", "Форум", "Интернет-магазин"]
+    ],
 
 
-let tipSaita = [" 1 - Сайт-визитка", " 2 - Форум", " 3 - Интернет-магазин"];
-prompt("Выбери тип сайта: " + tipSaita);
-let tipSaita2 = [];
-
-{
-    if (tipSaita == 1)
-    tipSaita2 = "Сайт-визитка",
-    y = 4444;
-}
-{
-    if (tipSaita == 2)
-    tipSaita2 = "Форум",
-    y = 5555;
-}
-{
-    if (tipSaita == 3)
-    tipSaita2 = "Интернет-магазин",
-    y = 6666;
-}
-
-
-let design = [" 1 - Свой макет", " 2 - Создание нового"];
-prompt("Выбери тип дизайна: " + design);
-let design2 = [];
-
-    if (design == 1)
-    design2 = "Свой макет",
-    z = 0;
-
-
-    if (design == 2)
-    design2 = "Создание нового",
-    z = 9999;
-
-
-alert("Тип верстки: " + verstka2 + " Тип сайта: " + tipSaita2 + " Дизайн: " + design2);
-
-let summa = (x + y + z) * 20;
-alert("Стоимость: " + summa + " рублей.");
-
-};
-
-/*
-let tipSaita = prompt("Выбери тип сайта | 1 - Сайт-визитка | 2 - Форум | 3 - Интернет-магазин");
-
-{
-
-    {
-        if (tipSaita == 1);
-        tipSaita = "Сайт-визитка",
-        y = 4444;
-    }
-
-    {
-        if (tipSaita == 2)
-        tipSaita = "Форум",
-        y = 5555;
-    }
-
-    {
-        if (tipSaita == 3)
-        tipSaita = "Интернет-магазин",
-        y = 6666;
-    }
-
+    tseniDesign: 
+    [
+    ver5 = ["Свой макет", "Новый макет"]
+    ],
 
 }
 
-let design = prompt("Выбери дизайн: | 1 - Свой макет | 2 - Создание нового");
-{
-
-    {
-        if (design == 1)
-        design = "Свой макет",
-        z = 0;
-    }
-
-    {
-        if (design == 2)
-        design = "Создание нового",
-        z = 9999;
-    }
-
+let sroki = {
+        srokVerstka:
+        [
+            ver2 = [7, 5, 3]
+        ],
+    
+        srokSait:
+        [
+            ver4 = [3, 5, 7]
+        ],
+    
+        srokDesign:
+        [
+        ver6 = [0, 7]
+        ],
 }
 
 
-let summa = (x + y + z) * 20;
+let verstka12 = prompt("Выбери тип верстки: " + ver1);
+let nameSait = prompt("Выбери тип сайта: " + ver3);
+let nameDesign = prompt("Выбери тип дизайна: " + ver5);
+
+if (verstka12 == this.ver1 + this.ver3 + this.ver5);
+alert(" Тип верстки: " + verstka12 + " Тип сайта: " + nameSait + " Тип дизайна: " + nameDesign);
+
+
+
+if (verstka12 == this.ver1, this.ver3, this.ver5);
+    console.log(verstka12)
+
+ver1[0] = 6666,
+ver1[1] = 4444,
+ver1[2] = 2222,
+
+ver3[0] = 2222,
+ver3[1] = 4444,
+ver3[2] = 6666,
+    
+ver5[0] = 0,
+ver5[1] = 6666
+
+// Стоимость
+let c, v, b;
+
+{
+    if(verstka12 == "Адаптивная")
+{
+    c = ver1[0]
+}
+
+if(verstka12 == "Фиксированная")
+{
+    c = ver1[1]
+}
+
+if(verstka12 == "Резиновая")
+{
+    c = ver1[2]
+}
+
+if(nameSait == "Сайт-визитка")
+{
+    v = ver3[0]
+}
+
+if(nameSait == "Форум")
+{
+    v = ver3[1]
+}
+
+if(nameSait == "Интернет-магазин")
+{
+    v = ver3[2]
+}
+
+if(nameDesign == "Свой макет")
+{
+    b = ver5[0]
+}
+
+if(nameDesign == "Новый макет")
+{
+    b = ver5[1]
+}
+}
+
+let summa = (c + v + b) * 20;
 
 console.log(summa);
 
-alert("Тип верстки: " + verstka + " Тип сайта: " + tipSaita + " Дизайн: " + design);
-alert("Стоимость: " + summa + " рублей.");
+let z, y, x;
 
-console.log("Тип верстки: " + verstka + " Тип сайта: " + tipSaita + " Дизайн: " + design);
-*/
+
+
+// Сроки
+{
+if(verstka12 == "Адаптивная")
+{
+    z = ver2[0]
+}
+
+if(verstka12 == "Фиксированная")
+{
+    z = ver2[1]
+}
+
+if(verstka12 == "Резиновая")
+{
+    z = ver2[2]
+}
+
+if(nameSait == "Сайт-визитка")
+{
+    y = ver2[0]
+}
+
+if(nameSait == "Форум")
+{
+    y = ver2[1]
+}
+
+if(nameSait == "Интернет-магазин")
+{
+    y = ver2[2]
+}
+
+if(nameDesign == "Свой макет")
+{
+    x = ver2[0]
+}
+
+if(nameDesign == "Новый макет")
+{
+    x = ver2[1]
+}
+
+let srokiObs;
+let stoimost;
+
+let faster = prompt("Срочность: 1 - Быстро | 2 - Обычно");
+
+if (faster == 1)
+{
+    stoimost = (summa * 2)
+    srokiObs = (z + x + y) / 2
+}
+
+if (faster == 2)
+{
+    stoimost = summa
+    srokiObs = (z + x + y)
+}
+
+alert("Сроки выполнения: " + Math.round(srokiObs) + " дней.");
+alert (stoimost);
+}
+
+}
